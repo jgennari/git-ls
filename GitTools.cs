7d153c10be7b81167eb6ec88c65212ef855426ab
Joey Gennari <joe@proteamagency.com>
2013-09-14 08:07:18 -0400

    Moved Git stuff to folder, added parallel processing

---
+++ GitTools.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace gitls
+{
+    public enum GitFileStatus
+    {
+        None,
+        Added,
+        Deleted,
+        Updated,
+        Copied,
+        Renamed,
+        Modified,
+        Unmodified,
+        Untracked,
+        Comment
+    }
+
+    public enum GitBranchStatus
+    {
+        Initial,
+        Ahead,
+        Behind,
+        UpToDate
+    }
+
+    public class GitDirectory
+    {
+        public string Path { get; set; }
+        public IEnumerable<GitFileStatus> Statuses { get; set; }
+        public bool IsGitDirectory { get; set; }
+        public GitBranchStatus BranchStatus { get; private set; }
+        public string BranchName { get; private set; }
+
+        public int AddedIndexCount { get; private set; }
+        public int ModifiedIndexCount { get; private set; }
+        public int DeletedIndexCount { get; private set; }
+        public int AddedWorkTreeCount { get; private set; }
+        public int ModifiedWorkTreeCount { get; private set; }
+        public int DeletedWorkTreeCount { get; private set; }
+        public int UntrackedCount { get; private set; }
+
+        public GitDirectory(string path)
+        {
+            this.Path = path;
+            if (Directory.Exists(System.IO.Path.Combine(path, ".git")))
+            {
+                this.IsGitDirectory = true;
+                // Fire a proccess to get the git status
+                Process pStatus = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        WorkingDirectory = path,
+                        FileName = "git.exe",
+                        Arguments = "status -s -b" // -s = short, -b = branch
+                    }
+                };
+
+                pStatus.Start();
+                var outputStatus = pStatus.StandardOutput.ReadToEnd().Split(Environment.NewLine.ToCharArray()).Select(l => new GitStatusEntry(l));
+                pStatus.WaitForExit();
+
+                // Branch name and status starts with ##
+                var branch = outputStatus.Single(s => s.IndexStatus == GitFileStatus.Comment).Value;
+
+                // Look for the ahead/behind keywords and set the color, also eliminate the trailing text
+                if (branch.Contains("Initial commit on"))
+                {
+                    branch = branch.Replace("Initial commit on ", "");
+                    this.BranchStatus = GitBranchStatus.Initial;
+                }
+                else if (branch.Contains("...") && branch.Contains("[ahead"))
+                {
+                    branch = branch.Substring(0, branch.IndexOf("..."));
+                    this.BranchStatus = GitBranchStatus.Ahead;
+                }
+                else if (branch.Contains("...") && branch.Contains("[behind"))
+                {
+                    branch = branch.Substring(0, branch.IndexOf("..."));
+                    this.BranchStatus = GitBranchStatus.Behind;
+                }
+                else
+                {
+                    this.BranchStatus = GitBranchStatus.UpToDate;
+                }
+
+                this.BranchName = branch;
+
+                var added = new[] { GitFileStatus.Added };
+                var modified = new[] { GitFileStatus.Modified, GitFileStatus.Renamed, GitFileStatus.Updated, GitFileStatus.Copied };
+                var deleted = new[] { GitFileStatus.Deleted };
+
+                // Staged changes
+                this.AddedIndexCount = outputStatus.Where(s => added.Contains(s.IndexStatus)).Count();
+                this.ModifiedIndexCount = outputStatus.Where(s => modified.Contains(s.IndexStatus)).Count();
+                this.DeletedIndexCount = outputStatus.Where(s => deleted.Contains(s.IndexStatus)).Count();
+
+                // Work tree changes
+                this.AddedWorkTreeCount = outputStatus.Where(s => added.Contains(s.WorkTreeStatus)).Count();
+                this.ModifiedWorkTreeCount = outputStatus.Where(s => modified.Contains(s.WorkTreeStatus)).Count();
+                this.DeletedWorkTreeCount = outputStatus.Where(s => deleted.Contains(s.WorkTreeStatus)).Count();
+
+                this.UntrackedCount = outputStatus.Where(s => s.IndexStatus == GitFileStatus.Untracked).Count();
+            }
+            else
+                this.IsGitDirectory = false;
+        }
+    }
+
+    class GitStatusEntry
+    {
+        public GitStatusEntry(string line)
+        {
+            if (line.Length > 3)
+            {
+                this.IndexStatus = ParseGitStatus(line.Substring(0, 1));
+                this.WorkTreeStatus = ParseGitStatus(line.Substring(1, 2));
+                this.Value = line.Substring(3);
+            }
+        }
+
+        public GitFileStatus IndexStatus { get; set; }
+        public GitFileStatus WorkTreeStatus { get; set; }
+
+        public string Value { get; set; }
+
+        public static GitFileStatus ParseGitStatus(string status)
+        {
+            if (status.Length < 1)
+                return GitFileStatus.None;
+
+            if (status.Substring(0, 1) == "#")
+                return GitFileStatus.Comment;
+
+            switch (status.Substring(0, 1))
+            {
+                case "M":
+                    return GitFileStatus.Modified;
+                case "D":
+                    return GitFileStatus.Deleted;
+                case "U":
+                    return GitFileStatus.Updated;
+                case "C":
+                    return GitFileStatus.Copied;
+                case "A":
+                    return GitFileStatus.Added;
+                case "R":
+                    return GitFileStatus.Renamed;
+                case "?":
+                    return GitFileStatus.Untracked;
+                case " ":
+                    return GitFileStatus.Unmodified;
+                default:
+                    return GitFileStatus.None;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,68 +8,6 @@ using System.Diagnostics;
 
 namespace gitls
 {
-    public enum GitStatus
-    {
-        None,
-        Added,
-        Deleted,
-        Updated,
-        Copied,
-        Renamed,
-        Modified,
-        Unmodified,
-        Untracked,
-        Comment
-    }
-
-    class GitStatusEntry
-    {
-        public GitStatusEntry(string line)
-        {
-            if (line.Length > 3)
-            {
-                this.IndexStatus = ParseGitStatus(line.Substring(0, 1));
-                this.WorkTreeStatus = ParseGitStatus(line.Substring(1, 2));
-                this.Value = line.Substring(3);
-            }
-        }
-
-        public GitStatus IndexStatus { get; set; }
-        public GitStatus WorkTreeStatus { get; set; }
-
-        public string Value { get; set; }
-        
-        public static GitStatus ParseGitStatus(string status)
-        {
-            if (status.Length < 1)
-                return GitStatus.None;
-
-            if (status.Substring(0, 1) == "#")
-                return GitStatus.Comment;
-
-            switch (status.Substring(0, 1))
-            {
-                case "M":
-                    return GitStatus.Modified;
-                case "D":
-                    return GitStatus.Deleted;
-                case "U":
-                    return GitStatus.Updated;
-                case "C":
-                    return GitStatus.Copied;
-                case "A":
-                    return GitStatus.Added;
-                case "R":
-                    return GitStatus.Renamed;
-                case "?":
-                    return GitStatus.Untracked;                    
-                case " ":
-                    return GitStatus.Unmodified;
-                default:
-                    return GitStatus.None;
-            }
-        }
-    }
 
     class Program
     {
@@ -93,69 +31,18 @@ namespace gitls
                 var dirs = Directory.GetDirectories(dir, "*", search);
 
                 // Find each directory where a .git subdirectory exists
-                foreach (var subDirectory in dirs)
+                Parallel.ForEach(dirs.Select(d => new GitDirectory(d)), subdir =>
                 {
-                    if (Directory.Exists(Path.Combine(subDirectory, ".git")))
+
+                    if (subdir.IsGitDirectory)
                     {
-                        // Fire a proccess to get the git status
-                        Process pStatus = new Process
-                        {
-                            StartInfo = new ProcessStartInfo
-                            {
-                                UseShellExecute = false,
-                                RedirectStandardOutput = true,
-                                WorkingDirectory = subDirectory,
-                                FileName = "git.exe",
-                                Arguments = "status -s -b" // -s = short, -b = branch
-                            }
-                        };
+                        // Output the directory name
+                        Console.Write(subdir.Path);
 
-                        pStatus.Start();
-                        var outputStatus = pStatus.StandardOutput.ReadToEnd().Split(Environment.NewLine.ToCharArray()).Select(l => new GitStatusEntry(l));
-                        pStatus.WaitForExit();
-
-                        // Branch name and status starts with ##
-                        var branch = outputStatus.Single(s => s.IndexStatus == GitStatus.Comment).Value;
-                    
                         // Save the console color before we change it
                         var firstcolor = Console.ForegroundColor;
 
                         var branchcolor = ConsoleColor.Cyan;
-
-                        // Look for the ahead/behind keywords and set the color, also eliminate the trailing text
-                        if (branch.Contains("Initial commit on"))
-                        {
-                            branch = branch.Replace("Initial commit on ", "");
-                        }
-                        else if (branch.Contains("...") && branch.Contains("[ahead"))
-                        {
-                            branch = branch.Substring(0, branch.IndexOf("..."));
-                            branchcolor = ConsoleColor.Green;
-                        }
-                        else if (branch.Contains("...") && branch.Contains("[behind"))
-                        {
-                            branch = branch.Substring(0, branch.IndexOf("..."));
-                            branchcolor = ConsoleColor.Red;
-                        }
-
-                        var added = new[] { GitStatus.Added };
-                        var modified = new[] { GitStatus.Modified, GitStatus.Renamed, GitStatus.Updated, GitStatus.Copied };
-                        var deleted = new[] { GitStatus.Deleted };
-
-                        // Staged changes
-                        var addedindexcount = outputStatus.Where(s => added.Contains(s.IndexStatus)).Count();
-                        var modifiedindexcount = outputStatus.Where(s => modified.Contains(s.IndexStatus)).Count();
-                        var deletedindexcount = outputStatus.Where(s => deleted.Contains(s.IndexStatus)).Count();
-
-                        // Work tree changes
-                        var addedworkcount = outputStatus.Where(s => added.Contains(s.WorkTreeStatus)).Count();
-                        var modifiedworkcount = outputStatus.Where(s => modified.Contains(s.WorkTreeStatus)).Count();
-                        var deletedworkcount = outputStatus.Where(s => deleted.Contains(s.WorkTreeStatus)).Count();
-
-                        var untrackedcount = outputStatus.Where(s => s.IndexStatus == GitStatus.Untracked).Count();
-                    
-                        // Output the directory name
-                        Console.Write(subDirectory);
 
                         // Change the color of the brackets
                         Console.ForegroundColor = ConsoleColor.Yellow;
@@ -163,27 +50,27 @@ namespace gitls
 
                         // Write the branch based on the color decided above
                         Console.ForegroundColor = branchcolor;
-                        Console.Write(branch);
+                        Console.Write(subdir.BranchName);
 
                         // If any files in the index are modified
-                        if (addedindexcount + modifiedindexcount + deletedworkcount > 0)
+                        if (subdir.AddedIndexCount + subdir.ModifiedIndexCount + subdir.DeletedIndexCount > 0)
                         {
                             Console.ForegroundColor = ConsoleColor.DarkGreen;
-                            Console.Write(string.Format(" +{0} ~{1} -{2}", addedindexcount, modifiedindexcount, deletedworkcount));
+                            Console.Write(string.Format(" +{0} ~{1} -{2}", subdir.AddedIndexCount, subdir.ModifiedIndexCount, subdir.DeletedIndexCount));
                         }
 
                         // If files in both the work tree and index are modified, introduce a seporator
-                        if (addedworkcount + modifiedworkcount + deletedworkcount + untrackedcount > 0 && addedindexcount + modifiedindexcount + deletedworkcount > 0)
+                        if (subdir.AddedWorkTreeCount + subdir.ModifiedWorkTreeCount + subdir.DeletedWorkTreeCount + subdir.UntrackedCount > 0 && subdir.AddedIndexCount + subdir.ModifiedIndexCount + subdir.DeletedIndexCount > 0)
                         {
                             Console.ForegroundColor = ConsoleColor.Yellow;
                             Console.Write(" |");
                         }
 
                         // If any files in the work tree are modified
-                        if (addedworkcount + modifiedworkcount + deletedworkcount + untrackedcount > 0)
+                        if (subdir.AddedWorkTreeCount + subdir.ModifiedWorkTreeCount + subdir.DeletedWorkTreeCount + subdir.UntrackedCount > 0)
                         {
                             Console.ForegroundColor = ConsoleColor.DarkRed;
-                            Console.Write(string.Format(" +{0} ~{1} -{2}", addedworkcount + untrackedcount, modifiedworkcount, deletedworkcount));
+                            Console.Write(string.Format(" +{0} ~{1} -{2}", subdir.AddedWorkTreeCount + subdir.UntrackedCount, subdir.ModifiedWorkTreeCount, subdir.DeletedWorkTreeCount));
                         }
 
                         // End the branches
@@ -192,12 +79,12 @@ namespace gitls
 
                         // Reset the color for the user
                         Console.ForegroundColor = firstcolor;
-                    }                    
+                    }
                     else if (args.Contains("-e"))
                     {
-                        Console.WriteLine(subDirectory);
+                        Console.WriteLine(subdir.Path);
                     }
-                }
+                });
             }
         }
     }
